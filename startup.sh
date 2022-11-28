e9dc7c1b8dc3cbd262c099bbebad7eea8f5e6334
styxiik <84225473+styxiik@users.noreply.github.com>
2022-11-28 14:09:51 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -7,6 +7,6 @@ echo > /run.log
 echo > /etc/crontabs/root
 rm -f /result.csv
 echo "30 */$HOURS * * * /bin/bash /run.sh $ZONE $DNSRECORD $CLOUDFLARE_AUTH_KEY > /run.log 2>&1" >> /etc/crontabs/root
-/usr/sbin/crond -i
+/usr/sbin/crond -b
 /bin/bash /run.sh $ZONE $DNSRECORD $CLOUDFLARE_AUTH_KEY
 tail -f /run.log
