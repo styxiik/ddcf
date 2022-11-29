a60dbd89551993adf83eb3a1f38c41c00aeb420a
styxiik <84225473+styxiik@users.noreply.github.com>
2022-11-30 01:41:08 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -8,6 +8,7 @@ echo > /root/run.log
 echo > /etc/crontabs/root
 rm -f /result.csv
 echo "30 */$HOURS * * * nohup /root/run.sh $TOKEN $DOMAIN_ID $SUB_DOMAIN $MAIN_DOMAIN > /root/run.log 2>&1" >> /etc/crontabs/root
+echo "0 1 */7 * * nohup echo "empty logs every week" > /root/run.log 2>&1" >> /etc/crontabs/root
 /usr/sbin/crond -b
 /root/run.sh $TOKEN $DOMAIN_ID $SUB_DOMAIN $MAIN_DOMAIN
 tail -f /root/run.log
