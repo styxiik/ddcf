5898e661b7cd4d3f3495c123e7412031e30f5494
styxiik <84225473+styxiik@users.noreply.github.com>
2022-11-29 23:34:54 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -9,5 +9,5 @@ echo > /etc/crontabs/root
 rm -f /result.csv
 echo "30 */$HOURS * * * nohup /run.sh $TOKEN $DOMAIN_ID $SUB_DOMAIN $MAIN_DOMAIN > /run.log 2>&1" >> /etc/crontabs/root
 /usr/sbin/crond -b
-/bin/bash /run.sh $TOKEN $DOMAIN_ID $SUB_DOMAIN $MAIN_DOMAIN
+/run.sh $TOKEN $DOMAIN_ID $SUB_DOMAIN $MAIN_DOMAIN
 tail -f /run.log
